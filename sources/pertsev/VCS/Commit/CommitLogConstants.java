9140046a704e8b9c3438bfa0f60656455f244f48
RomanPepper <70803548+RomanPepper@users.noreply.github.com>
2023-10-23 22:59:37 +0300

    Deleted CommitLogFileHandler(it is redunant) and constants transferred to CommitLogConstants

---
+++ sources/pertsev/VCS/Commit/CommitLogConstants.java
@@ -0,0 +1,6 @@
+package pertsev.VCS.Commit;
+
+class CommitLogConstants {
+    protected static final String COMMIT_SEPARATOR = "------COMMIT_END------";
+    protected static final String CHANGE_SET_SEPARATOR = "---CHANGESET_END---";
+}
